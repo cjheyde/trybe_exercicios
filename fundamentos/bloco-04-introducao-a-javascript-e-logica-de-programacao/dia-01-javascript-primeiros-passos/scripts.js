6de74b3f56ade17bbfcee1e209347d3c277a2d62
Carla Heyde <carlaheyde@gmail.com>
2021-11-26 20:47:07 -0300

    Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,11 +1,34 @@
-const myName = "Carla Heyde";
-const birthCity = "São Paulo - SP";
-let birthYear = 1969;
+//const myName = "Carla Heyde";
+//const birthCity = "São Paulo - SP";
+//let birthYear = 1969;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
-console.log(myName,",",birthCity,",",birthYear);
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
+//console.log(myName,",",birthCity,",",birthYear);
 
-birthYear = 2030;
-console.log(birthYear);
+//birthYear = 2030; 
+//console.log(birthYear);
+
+let patientId = "50";
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientAge);
+
+let base = 5;
+let height = 8;
+
+console.log(base, height);
+
+let area = base * height;
+console.log(area);
+
+let perimeter = 2*base + 2*height;
+
+console.log(perimeter);
