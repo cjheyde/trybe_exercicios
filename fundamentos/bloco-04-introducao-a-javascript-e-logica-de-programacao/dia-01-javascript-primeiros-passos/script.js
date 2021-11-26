962cd7ded78865001d0d150ab658f9b0978c2ee9
Carla Heyde <carlaheyde@gmail.com>
2021-11-26 19:28:19 -0300

    completei o exercício

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,11 @@
+const myName = "Carla Heyde";
+const birthCity = "São Paulo - SP";
+let birthYear = 1969;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+console.log(myName,",",birthCity,",",birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
