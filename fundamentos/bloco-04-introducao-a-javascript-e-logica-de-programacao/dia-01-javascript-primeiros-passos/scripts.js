a86210fb28e1075ea2a3404f09a53b370926b724
Carla Heyde <carlaheyde@gmail.com>
2021-11-26 21:21:38 -0300

    Organizando os exercicios com linha de titulo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,4 @@
+//Aprendendo a declarar variáveis
 //const myName = "Carla Heyde";
 //const birthCity = "São Paulo - SP";
 //let birthYear = 1969;
@@ -10,6 +11,8 @@
 //birthYear = 2030; 
 //console.log(birthYear);
 
+
+//usando o typeof para descobrir o tipo da variavel
 //let patientId = "50";
 //let isEnrolled = true;
 //const patientInfo = {
@@ -21,6 +24,8 @@
 //console.log(typeof patientId);
 //console.log(typeof patientAge);
 
+
+//calculo no console log
 //let base = 5;
 //let height = 8;
 
@@ -33,6 +38,8 @@
 
 //console.log(perimeter);
 
+
+//IF&Else
 //const notaDesafioTecnico = 48;
 //if (notaDesafioTecnico >= 80) {
   //  console.log("Parabéns, você foi aprovada(o)!");
@@ -42,23 +49,29 @@
 // }
 // else console.log("Você foi reprovada(o)");
 
-const currentHour = 4;
-let message = "";
 
-if (currentHour >= 22) {
-    message = "Não deveríamos comer nada, é hora de dormir";
-}
-else if (currentHour >= 18) {
-    message = "Rango da noite, vamos jantar :D";
-}
-else if (currentHour >= 14) {
-    message = "Vamos fazer um bolo pro café da tarde?";
-}
-else if (currentHour >= 11) {
-    message = "Hora do almoço!!!";
-}
-else if (currentHour >= 4) {
-    message = "Hmmm, cheiro de café recém passado";
-}
+// Operador lógico && AND
+// const currentHour = 4;
+// let message = "";
 
-console.log(message);
+// if (currentHour >= 22) {
+//     message = "Não deveríamos comer nada, é hora de dormir";
+// }
+// else if (currentHour >= 18) {
+//     message = "Rango da noite, vamos jantar :D";
+// }
+// else if (currentHour >= 14) {
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// }
+// else if (currentHour >= 11) {
+//     message = "Hora do almoço!!!";
+// }
+// else if (currentHour >= 4) {
+//     message = "Hmmm, cheiro de café recém passado";
+// }
+
+// console.log(message);
+
+
+// Operado Lógico - || OR OU
+
