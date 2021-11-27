adacd3050c5433f8f296bf584d3482c35a9b2c73
Carla Heyde <carlaheyde@gmail.com>
2021-11-26 23:31:01 -0300

    Operado logico OU ||

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -74,4 +74,11 @@
 
 
 // Operado Lógico - || OR OU
+let weekDay = "sabado";
 
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
