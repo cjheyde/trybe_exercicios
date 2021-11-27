cab7a8609de646dfa9d05d53b06adef8979a986a
Carla Heyde <carlaheyde@gmail.com>
2021-11-26 21:16:10 -0300

    Operadores lógicos-if else

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -33,12 +33,32 @@
 
 //console.log(perimeter);
 
-const notaDesafioTecnico = 48;
-if (notaDesafioTecnico >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!");
-}
-else if (notaDesafioTecnico >= 60) {
-    console.log("Você está na nossa lista de espera");
-}
-else console.log("Você foi reprovada(o)");
+//const notaDesafioTecnico = 48;
+//if (notaDesafioTecnico >= 80) {
+  //  console.log("Parabéns, você foi aprovada(o)!");
+// }
+// else if (notaDesafioTecnico >= 60) {
+//     console.log("Você está na nossa lista de espera");
+// }
+// else console.log("Você foi reprovada(o)");
 
+const currentHour = 4;
+let message = "";
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir";
+}
+else if (currentHour >= 18) {
+    message = "Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14) {
+    message = "Vamos fazer um bolo pro café da tarde?";
+}
+else if (currentHour >= 11) {
+    message = "Hora do almoço!!!";
+}
+else if (currentHour >= 4) {
+    message = "Hmmm, cheiro de café recém passado";
+}
+
+console.log(message);
