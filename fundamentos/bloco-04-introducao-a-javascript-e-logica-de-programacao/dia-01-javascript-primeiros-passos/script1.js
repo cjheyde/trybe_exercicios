d79c2062bfe8c9fd6b92d7e671297c0367b91bf6
Carla Heyde <carlaheyde@gmail.com>
2021-11-27 12:32:49 -0300

    exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script1.js
@@ -1,4 +1,4 @@
-// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+// 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 // Adição (a + b)
 // Subtração (a - b)
 // Multiplicação (a * b)
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script2.js
@@ -0,0 +1,16 @@
+// 2. Faça um programa que retorne o maior de dois números. 
+// Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 5;
+const b = 10;
+
+if (a > b) {
+    console.log(a, "é o maior número");
+}
+else if (a === b) {
+    console.log("os dois números são iguais");
+}
+else {
+    console.log(b, "é o maior número");
+}
+
