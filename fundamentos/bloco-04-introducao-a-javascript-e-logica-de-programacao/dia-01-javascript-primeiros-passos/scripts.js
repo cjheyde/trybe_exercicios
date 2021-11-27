ce12c6a217c59abbf01105b8fe89a63f3198d8cc
Carla Heyde <carlaheyde@gmail.com>
2021-11-27 11:42:32 -0300

    Switch case

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -74,17 +74,33 @@
 
 
 // Operado Lógico - || OR OU
-let weekDay = "sabado";
+// let weekDay = "sabado";
 
-if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
-    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
-}
-else {
-    console.log("FINALMENTE, descanso merecido UwU");
-}
+// if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+//     console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+// }
+// else {
+//     console.log("FINALMENTE, descanso merecido UwU");
+// }
 
 
 // Switch e Case
-// Crie uma variável para armazenar o estado da pessoa candidata no processo seletivo, que pode ser: 'aprovada' , 'lista' ou 'reprovada' ;
-// Crie uma estrutura condicional com o switch/case que irá imprimir as mensagens do exercício anterior se o estado da pessoa candidata for 'aprovada' , 'lista' ou 'reprovada' . Como default , imprima a mensagem de 'não se aplica' .
-// A estrutura switch/case é utilizada quando queremos executar diferentes ações. A expressão que passamos para o switch é avaliada apenas uma vez, e o seu valor é comparado em cada caso. Se essa comparação for verdadeira, o código do caso avaliado é executado. Se nenhum valor satisfizer os casos listados, é executado o código em default .
+let estadoCandidato = "";
+
+switch (estadoCandidato) {
+  case "aprovada":
+  console.log('aprovada');
+  break;
+
+  case 'lista': 
+  console.log('lista');
+  break;
+
+  case 'reprovada':
+  console.log('reprovada');
+  break;
+
+  default:
+  console.log('não se aplica');
+}
+
