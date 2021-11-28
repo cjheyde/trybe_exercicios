5eb6f0cb274c37761e229cb78d048fd5fae51f0f
Carla Heyde <carlaheyde@gmail.com>
2021-11-27 21:25:05 -0300

    exercicio 8

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script8.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script8.js
@@ -3,4 +3,16 @@
 // Caso contrário, ele retorna false .
 // Bonus: use somente um if .
 
+const a = 9;
+const b = 5;
+const c = 3;
+let resultado = "";
 
+if (a%2 === 0 || b%2 === 0 || c%2 === 0) {
+    resultado = "True";
+}
+else {
+    resultado = "False";
+}
+
+console.log(resultado);
