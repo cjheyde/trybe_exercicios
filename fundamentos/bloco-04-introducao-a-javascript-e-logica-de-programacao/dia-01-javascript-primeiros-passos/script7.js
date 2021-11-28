4ec6a483e3245b16ce3a182915b698b64efb12bc
Carla Heyde <carlaheyde@gmail.com>
2021-11-27 21:16:51 -0300

    exercicio 7

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script7.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script7.js
@@ -9,3 +9,29 @@
 // O programa deve retornar uma mensagem de erro e encerrar se a nota passada 
 // for menor que 0 ou maior que 100.
 
+let nota = 700;
+let resultado = "";
+
+if (nota > 100 || nota < 0) {
+    resultado = "Erro - Essa nota não está dentro da faixa de 0 a 100, favor verificar e repetir!";
+}
+else if (nota >= 90 && nota <= 100) {
+    resultado = "A";
+}
+else if (nota >= 80) {
+    resultado = "B";
+}
+else if (nota >= 70) {
+    resultado = "C";
+}
+else if (nota >= 60) {
+    resultado = "D";
+}
+else if (nota >= 50) {
+    resultado = "E";
+}
+else {
+    resultado = "D";
+}
+
+console.log(resultado);
