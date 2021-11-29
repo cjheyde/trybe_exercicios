91c8cd29956133789d1c6db4d6950add477552db
Carla Heyde <carlaheyde@gmail.com>
2021-11-29 17:03:15 -0300

    exemplos aula 4.2 para Arrays (lista)

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/array.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/array.js
@@ -8,3 +8,12 @@ console.log(searchForFirstTask);
 let searchForLastTask = tasksList[tasksList.length - 1];
 console.log(searchForLastTask);
 
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+tasksList.pop();  // remove a última tarefa
+console.log(tasksList);
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);
+
