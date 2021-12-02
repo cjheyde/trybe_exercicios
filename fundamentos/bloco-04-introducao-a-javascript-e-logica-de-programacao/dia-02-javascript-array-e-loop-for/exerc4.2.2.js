da6a5edd34cd2cd080b9451f6c2839b0fb2ce8b9
Carla Heyde <carlaheyde@gmail.com>
2021-12-02 01:26:35 -0300

    exerc. 4.2.2 & 4.2.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exerc4.2.2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exerc4.2.2.js
@@ -1,16 +1,7 @@
 // Procure o índice do valor "Portfólio" do array menu :
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menu = ["Home", "Serviços", "Portfólio", "Links"];
 let indexOfPortfolio;
 
-for (let index = 0; index < menu.length; index ++) {
-    if (menu[index] === "Portfólio") {
-        indexOfPortfolio = index;
-    }
-    else {
-        indexOfPortfolio = "Não existe esse dado no Array"
-    }
-    console.log(menu[index]);
-}
-
+indexOfPortfolio = menu.indexOf("Portfólio")
 console.log(indexOfPortfolio);
