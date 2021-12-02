8d6e5a0b21167cf9bfd9a9ade35f4722d4354b9b
Carla Heyde <carlaheyde@gmail.com>
2021-12-02 01:52:02 -0300

    exerc. 4.2.4 & 4.2.5

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exerc4.2.4.js
@@ -0,0 +1,6 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// 4 - Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
