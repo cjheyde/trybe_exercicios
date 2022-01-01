a2f88dc82f453a72d3d6f5cb810fa06baf7a9671
cjheyde <carlaheyde@gmail.com>
2022-01-01 17:57:45 -0300

    exercicio 5.2.2 - todos os itens

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-02-javascript-trabalhando-com-elementos/Exercicio 5.2.2/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-02-javascript-trabalhando-com-elementos/Exercicio 5.2.2/script.js
@@ -1,25 +1,26 @@
 // 1.Acesse o elemento elementoOndeVoceEsta .
- let elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+let elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
 
 // 2.Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 let pai = elementoOndeVoceEsta.parentElement;
 pai.style.color = "red";
 
-// 3.Acesse o primeiroFilhoDoFilho e adicione um texto a ele. 
+// 3.Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
 // Você se lembra dos vídeos da aula anterior, como fazer isso?
-let primeiroFilho = elementoOndeVoceEsta.firstElementChild.innerText = "primeiro filho do filho do elemento elementoOndeVoceEsta";
+let primeiroFilho = (elementoOndeVoceEsta.firstElementChild.innerText =
+  "primeiro filho do filho do elemento elementoOndeVoceEsta");
 
 // 4.Acesse o primeiroFilho a partir de pai .
-// const primeiroFilho = pai.firstElementChild;
+let primeiroFilho2 = pai.firstElementChild;
 
 // 5.Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
-// const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
+let primeiroFilho3 = elementoOndeVoceEsta.previousElementSibling;
 
 // 6.Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
-// const textElement = elementoOndeVoceEsta.nextSibling;
+let textElement = elementoOndeVoceEsta.nextSibling;
 
 // 7.Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
-// const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+let terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
 
-// 8.Agora acesse o terceiroFilho a partir de pai .1. 
-// const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+// 8.Agora acesse o terceiroFilho a partir de pai .1.
+let terceiroFilho2 = pai.lastElementChild.previousElementSibling;
