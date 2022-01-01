fefed02ae17ca967ece24bcc6ce575d0f19d7ce4
cjheyde <carlaheyde@gmail.com>
2022-01-01 12:20:13 -0300

    segundo exercicio

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-01-javascript-dom-e-seletores/Exercicio 5.1.1/script.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = "Caminhando nas Nuvens";
+
+document.getElementById("second-paragraph").innerText = "Sorria para a vida =)";
+
+document.getElementById("subtitle").innerHTML = "Feliz 2022";
