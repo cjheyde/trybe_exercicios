972266e539614474524b8745b459648bdc4a92a8
cjheyde <carlaheyde@gmail.com>
2022-01-01 16:52:58 -0300

    excercicio 5.1.5 4 primeiras funcOes

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-01-javascript-dom-e-seletores/exercicio 5.1.5/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-01-javascript-dom-e-seletores/exercicio 5.1.5/script.js
@@ -11,13 +11,30 @@
  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
         */
+function changeText() {
+    let visao = document.getElementsByTagName("p")[1].innerText =
+    "Em 2 anos estarei trabalhando como Desenvolvedora Web pleno, na Alemanha, ganhando " + 
+    "muito bem em Euros, e sustentando toda a minha família";
+    }
+changeText();
 
-const visao = document.getElementsByTagName("p")[0].innerText =
-  "Em 2 anos estarei trabalhando como Desenvolvedora Web pleno, na Alemanha, ganhando " + 
-  "muito bem em Euros, e sustentando toda a minha família";
+function changeBackColorGreen() {
+    let mainContent = document.getElementsByClassName("main-content")[0].style.background = "rgb(76,164,109)";
+}
+changeBackColorGreen();
 
-const autoria = document.getElementsByTagName("p")[2].innerText = "CJHeyde";
+function changeBackWhite() {
+    let centerContent = document.getElementsByClassName("center-content")[0].style.background = "white";
+}
+changeBackWhite();
 
-// let mainContent = document.getElementsByTagName("main-content").style.background-color = "yellow";
+function correcaoTexto() {
+    let textoH1 = document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
+}
+correcaoTexto();
 
-const textoH1 = document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
+function textoMaiusculo() {
+    let tagP = document.getElementsByTagName("p")[0].innerHTML.toUpperCase();
+}
+textoMaiusculo();
+
