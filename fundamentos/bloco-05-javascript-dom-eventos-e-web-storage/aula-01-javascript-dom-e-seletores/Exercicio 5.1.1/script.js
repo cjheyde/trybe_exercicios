f5d986fe5f8c90798658ee23ddbebf0561a6c936
cjheyde <carlaheyde@gmail.com>
2022-01-01 15:43:45 -0300

    update de todos os exercicios 5.1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-01-javascript-dom-e-seletores/Exercicio 5.1.1/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-01-javascript-dom-e-seletores/Exercicio 5.1.1/script.js
@@ -1,8 +1,25 @@
 const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
-document.getElementById("page-title").innerText = "Caminhando nas Nuvens";
+const title = document.getElementById("page-title").innerText = "Caminhando nas Nuvens";
 
-document.getElementById("second-paragraph").innerText = "Sorria para a vida =)";
+const secondParagraph = document.getElementById("second-paragraph").innerText = "Sorria para a vida =)";
 
-document.getElementById("subtitle").innerHTML = "Feliz 2022";
+const subtitle = document.getElementById("subtitle").innerHTML = "Feliz 2022";
+
+// const paragraph = document.getElementById('paragraph');
+//       paragraph.style.color = 'red';
+
+//       // 1- Recupere o elemento que contém o título da página e faça algo com ele,
+//       // como alterá-lo para o nome do seu filme favorito.
+//       const title = document.getElementById('page-title');
+//       title.innerText = 'The hitchhiker\'s guide to the galaxy';
+
+//       // 2 - Agora recupere o segundo parágrafo e use sua criatividade para
+//       //  alterá-lo.
+//       const secondParagraph = document.getElementById('second-paragraph');
+//       secondParagraph.innerText = 'The answer to life the universe and everything is 42.';
+
+//       // 3 - Por fim, recupere o subtítulo e altere-o também.
+//       const subtitle = document.getElementById('subtitle');
+//       subtitle.innerText = 'So long and thanks for all the fish';
---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-01-javascript-dom-e-seletores/Exercicio 5.1.4/script.js
@@ -0,0 +1,33 @@
+const header = document.querySelector("h1").style.backgroundColor = "rgb(0, 176, 105)";
+
+const emergencyTasks = document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = "rgb(253, 155, 135)";
+
+const emergencyTasksTitle1 = document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "rgb(212, 129, 228)";
+
+const emergencyTasksTitle2 = document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "rgb(212, 129, 228)";
+
+const foot = document.querySelector("footer").style.backgroundColor = "rgb(0, 58, 55)";
+
+const
+
+// const header = document.getElementById('header-container');
+// header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+// const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+// emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
+
+// const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+// for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+//   emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+// }
+
+// const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+// noEmergencyTasks.style.backgroundColor = 'rgb(249, 219, 94)';
+
+// const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+// for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+//   noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(35, 37, 37)';
+// }
+
+// const footer = document.getElementById('footer-container');
+// footer.style.backgroundColor = 'rgb(0, 53, 51)';
