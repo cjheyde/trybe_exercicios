3156590bcacd795bd21e6a5bd1fa14bd462cf94d
cjheyde <carlaheyde@gmail.com>
2022-01-01 22:03:33 -0300

    exercicio 5.3.1 itens 1 e 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-03-javascript-eventos/Exercicio 5.3.1/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/aula-03-javascript-eventos/Exercicio 5.3.1/script.js
@@ -8,9 +8,20 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// resp.: por causa do estilo da classe .tech, ligada ao primeiro quadrado, e que transform: translateY(-20px) 
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function addClasseTech (event){
+let techElement = document.querySelector(".tech");
+techElement.classList.remove("tech");
+event.target.classList.add("tech");
+input.value = "";
+}
+
+firstLi.addEventListener('click', addClasseTech);
+secondLi.addEventListener('click', addClasseTech);
+thirdLi.addEventListener('click', addClasseTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
