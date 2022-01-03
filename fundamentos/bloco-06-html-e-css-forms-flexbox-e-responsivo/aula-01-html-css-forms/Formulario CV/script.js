d834b73e6c7be25d9c518dd0ea9938c521a47280
cjheyde <carlaheyde@gmail.com>
2022-01-03 02:21:04 -0300

    exercicio formulario CV completo - a revisar js

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/aula-01-html-css-forms/Formulario CV/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/aula-01-html-css-forms/Formulario CV/script.js
@@ -1,3 +1,4 @@
+// script.js
 function createStateOptions() {
   let states = document.getElementById("state");
   let stateOptions = [
@@ -38,256 +39,302 @@ function createStateOptions() {
   }
 }
 
-window.onload = function () {
-    createStateOptions();
-    let submitButton = document.querySelector('.submit-button');
-    submitButton.addEventListener('click', handleSubmit);
-  
-    let clearButton = document.querySelector('.clear-button');
-    clearButton.addEventListener('click', clearFields)
+let inputs = {
+  name: {
+    maxLength: 40,
+    required: true,
+  },
+  email: {
+    maxLength: 50,
+    required: true,
+  },
+  cpf: {
+    maxLength: 11,
+    required: true,
+  },
+  address: {
+    maxLength: 200,
+    required: true,
+  },
+  city: {
+    maxLength: 28,
+    required: true,
+  },
+  state: {
+    type: "select",
+    required: true,
+  },
+  houseType: {
+    type: "radio",
+    required: true,
+  },
+  resume: {
+    maxLength: 1000,
+    required: true,
+  },
+  role: {
+    maxLength: 40,
+    required: true,
+  },
+  roleDescription: {
+    maxLength: 500,
+    required: true,
+  },
+  startDate: {
+    type: "date",
+    required: true,
+  },
+};
+
+function defaultValidation(input, name) {
+  let trimmed = input.value.trim();
+  let validation = inputs[name];
+
+  if (validation.required && trimmed.length === 0) {
+    return false;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  
-    let validation = validateData();
-  
-    clearDivs();
-    
-    if(validation.errorQtd === 0){
-      renderData();
-    } else {
-      validation.messages.unshift('Dados Inválidos')
-  
-      renderErrorMessages(validation.messages)
-    }
+  if (validation.maxLength && trimmed.length > validation.maxLength) {
+    return false;
   }
 
-  function validateData(){
-    let validationsList = {};
-  
-    for(let inputName in inputs){
-      let isValid = validateInput(inputName);
-      validationsList[inputName] = isValid;
-    }
-    
-    let counter = 0;
-    let messages = [];
-  
-    for(let index in validationsList){
-      if(validationsList[index] === false){
-        counter += 1;
-      }
-  
-      if(validationsList[index].message){
-        counter += 1;
-        messages.push(validationsList[index].message);
-      }
-    }
-  
+  return true;
+}
+
+function dateValidation(input, name) {
+  if (input.value.length === 0) {
     return {
-      errorQtd: counter,
-      messages,
-    }
+      message: "A data não foi preenchida!",
+    };
   }
 
-  function validateInput(inputName){
-    let inputType = inputs[inputName].type;
-    let input = document.querySelector(`[name=${inputName}]`)
-  
-    if(inputType){
-      let validationFunction = validationStrategies[inputType];
-      return validationFunction(input, inputName);
-    }
-  
-    return validationStrategies.default(input, inputName);
+  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
+
+  if (!regex.test(input.value)) {
+    return {
+      message: "Data: Formato inválido",
+    };
   }
 
-  let validationStrategies = {
-    default: defaultValidation,
-    date: dateValidation,
-    select: selectValidation,
-    radio: radioValidation,
+  let splitted = input.value.split("/");
+  let day = splitted[0];
+  let month = splitted[1];
+  let year = splitted[2];
+
+  if (day < 0 || day > 30) {
+    return {
+      message: "Dia inválido",
+    };
   }
 
-  function defaultValidation(input, name){
-    let trimmed = input.value.trim();
-    let validation = inputs[name];
-  
-    if(validation.required && trimmed.length === 0){
-      return false;
-    }
-  
-    if(validation.maxLength && trimmed.length > validation.maxLength){
-      return false;
-    }
-    
-    return true;
+  if (month < 0 || month > 12) {
+    return {
+      message: "Mês inválido",
+    };
   }
 
-  function dateValidation(input, name){
-    if(input.value.length === 0){
-      return {
-        message: 'A data não foi preenchida!'
-      }
-    }
-  
-    let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
-    
-    if(!regex.test(input.value)){
-      return {
-        message: 'Data: Formato inválido'
-      };
-    }
-  
-    let splitted = input.value.split('/');
-    let day = splitted[0];
-    let month = splitted[1];
-    let year = splitted[2];
-  
-    if(day < 0 || day > 30){
-      return {
-        message: 'Dia inválido'
-      };
-    }
-  
-    if(month < 0 || month > 12){
-      return {
-        message: 'Mês inválido'
-      }
-    }
-  
-    if(year < 0) {
-      return {
-        message: 'Ano inválido'
-      };
-    }
-  
-    return true;
+  if (year < 0) {
+    return {
+      message: "Ano inválido",
+    };
   }
 
-  function getSelectedOption(select){
-    return select.options[select.selectedIndex];
-  }
-  
-  function selectValidation(select, name){
-    let option = getSelectedOption(select);
-    let validation = inputs[name];
-  
-    if(validation.required && (!option || option.disabled)){
-      return false;
-    }
-  
-    return true;
+  return true;
+}
+
+function getSelectedOption(select) {
+  return select.options[select.selectedIndex];
+}
+
+function selectValidation(select, name) {
+  let option = getSelectedOption(select);
+  let validation = inputs[name];
+
+  if (validation.required && (!option || option.disabled)) {
+    return false;
   }
 
-  function radioValidation(radio, name){
-    let checked = document.querySelector(`[name=${name}]:checked`)
-    
-    if(checked === null){
-      return false;
+  return true;
+}
+
+function radioValidation(radio, name) {
+  let checked = document.querySelector(`[name=${name}]:checked`);
+
+  if (checked === null) {
+    return false;
+  }
+
+  return true;
+}
+
+let validationStrategies = {
+  default: defaultValidation,
+  date: dateValidation,
+  select: selectValidation,
+  radio: radioValidation,
+};
+
+function validateInput(inputName) {
+  let inputType = inputs[inputName].type;
+  let input = document.querySelector(`[name=${inputName}]`);
+
+  if (inputType) {
+    let validationFunction = validationStrategies[inputType];
+    return validationFunction(input, inputName);
+  }
+
+  return validationStrategies.default(input, inputName);
+}
+
+function renderErrorMessages(messages) {
+  let form = document.querySelector("#cv-form");
+  let messageDiv = document.createElement("div");
+  messageDiv.className = "error";
+  form.prepend(messageDiv);
+
+  for (let message of messages) {
+    let p = document.createElement("p");
+    p.innerText = message;
+
+    messageDiv.appendChild(p);
+  }
+}
+
+function defaultRendering(input) {
+  let p = document.createElement("p");
+  p.innerText = input.value;
+
+  return p;
+}
+
+function radioRendering(input) {
+  let p = document.createElement("p");
+  let name = input.getAttribute("name");
+  let checked = document.querySelector(`[name=${name}]:checked`);
+
+  if (checked) {
+    p.innerText = checked.value;
+  }
+
+  return p;
+}
+
+function selectRendering(input) {
+  let p = document.createElement("p");
+  let option = getSelectedOption(input);
+  p.innerText = option.value;
+
+  return p;
+}
+
+let renderStrategies = {
+  default: defaultRendering,
+  radio: radioRendering,
+  select: selectRendering,
+};
+
+function renderData() {
+  let dataDiv = document.createElement("div");
+  dataDiv.className = "data";
+
+  let form = document.querySelector("#cv-form");
+  form.prepend(dataDiv);
+
+  for (let name in inputs) {
+    let inputType = inputs[name].type;
+    let input = document.querySelector(`[name=${name}]`);
+
+    let element;
+
+    if (renderStrategies[inputType]) {
+      element = renderStrategies[inputType](input, dataDiv);
+    } else {
+      element = renderStrategies.default(input, dataDiv);
     }
-  
-    return true;
+
+    dataDiv.appendChild(element);
+  }
+}
+
+function validateData() {
+  let validationsList = {};
+
+  for (let inputName in inputs) {
+    let isValid = validateInput(inputName);
+    validationsList[inputName] = isValid;
   }
 
   let counter = 0;
-let messages = [];
+  let messages = [];
 
-for(let index in validationsList){
-  if(validationsList[index] === false){
-    counter += 1;
-  }
-
-  if(validationsList[index].message){
-    counter += 1;
-    messages.push(validationsList[index].message);
-  }
-}
-
-return {
-  errorQtd: counter,
-  messages,
-}
-
-function clearDivs(){
-    let errorDivs = document.querySelectorAll('.error');
-  
-    for(div of errorDivs){
-      div.remove();
+  for (let index in validationsList) {
+    if (validationsList[index] === false) {
+      counter += 1;
     }
-  
-    let dataDiv = document.querySelector('.data');
-  
-    if(dataDiv){
-      dataDiv.remove();
+
+    if (validationsList[index].message) {
+      counter += 1;
+      messages.push(validationsList[index].message);
     }
   }
 
-  if(validation.errorQtd === 0){
+  return {
+    errorQtd: counter,
+    messages,
+  };
+}
+
+function clearDivs() {
+  let errorDivs = document.querySelectorAll(".error");
+
+  for (div of errorDivs) {
+    div.remove();
+  }
+
+  let dataDiv = document.querySelector(".data");
+
+  if (dataDiv) {
+    dataDiv.remove();
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let validation = validateData();
+
+  clearDivs();
+
+  if (validation.errorQtd === 0) {
     renderData();
   } else {
-    validation.messages.unshift('Dados Inválidos')
+    validation.messages.unshift("Dados Inválidos");
 
-    renderErrorMessages(validation.messages)
+    renderErrorMessages(validation.messages);
   }
+}
 
-  function renderData(){
-    let dataDiv = document.createElement('div');
-    dataDiv.className = 'data';
-  
-    let form = document.querySelector('#cv-form');
-    form.prepend(dataDiv);
-  
-    for(let name in inputs){
-      let inputType = inputs[name].type;
-      let input = document.querySelector(`[name=${name}]`);
-  
-      let element;
-  
-      if(renderStrategies[inputType]){
-        element = renderStrategies[inputType](input, dataDiv)
-      } else {
-        element = renderStrategies.default(input, dataDiv)
-      }
-  
-      dataDiv.appendChild(element);
-    }
+function clearFields() {
+  let formElements = document.querySelectorAll("input");
+  let textArea = document.querySelector("textarea");
+  let div = document.querySelectorAll(".div-curriculum");
+  for (
+    let index = 0;
+    index < formElements.length && index < div.length;
+    index += 1
+  ) {
+    let userInput = formElements[index];
+    userInput.value = "";
+    textArea.value = "";
+    div[index].innerText = "";
   }
+}
 
-  function renderErrorMessages(messages){
-    let form = document.querySelector('#cv-form');
-    let messageDiv = document.createElement('div');
-    messageDiv.className = 'error';
-    form.prepend(messageDiv);
-  
-    for(let message of messages){
-      let p = document.createElement('p');
-      p.innerText = message;
-  
-      messageDiv.appendChild(p);
-    }
-  }
+window.onload = function () {
+  createStateOptions();
+  let submitButton = document.querySelector(".submit-button");
+  submitButton.addEventListener("click", handleSubmit);
 
-  window.onload = function () {
-    createStateOptions();
-    let submitButton = document.querySelector('.submit-button');
-    submitButton.addEventListener('click', handleSubmit);
-  
-    let clearButton = document.querySelector('.clear-button');
-    clearButton.addEventListener('click', clearFields)
-  }
-
-  function clearFields() {
-    let formElements = document.querySelectorAll('input');
-    let textArea = document.querySelector('textarea')
-    let div = document.querySelectorAll('.div-curriculum');
-    for (let index = 0; index < formElements.length && index < div.length; index += 1) {
-      let userInput = formElements[index];
-      userInput.value = '';
-      textArea.value = '';
-      div[index].innerText = '';
-    }
-  }
-
-  
+  let clearButton = document.querySelector(".clear-button");
+  clearButton.addEventListener("click", clearFields);
+};
