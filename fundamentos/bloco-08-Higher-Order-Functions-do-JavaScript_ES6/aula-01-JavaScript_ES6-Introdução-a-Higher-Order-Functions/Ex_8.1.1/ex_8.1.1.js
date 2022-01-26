897a5a05042406515809dad1b84d4683814c2cb2
cjheyde <carlaheyde@gmail.com>
2022-01-26 19:13:30 -0300

    melhoria do ex.8.1.1 e completo ex. 8.1.2

--- fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript_ES6/aula-01-JavaScript_ES6-Introdução-a-Higher-Order-Functions/Ex_8.1.1/ex_8.1.1.js
+++ fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript_ES6/aula-01-JavaScript_ES6-Introdução-a-Higher-Order-Functions/Ex_8.1.1/ex_8.1.1.js
@@ -1,21 +1,29 @@
 // 1 - Crie uma função que retorne a string 'Acordando!!' ;
-function acaoAcordar() {
-  console.log("Acordando!!");
-}
+const acaoAcordar = () => "Acordando!!";
+
 // 2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
-function acaoCafe() {
-    console.log("Bora tomar café!!");
-  }
+// function acaoCafe() {
+//     console.log("Bora tomar café!!");
+//   }
+  const acaoCafe = () => "Bora tomar café!!";
+
 // 3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
-function acaoDormir() {
-    console.log("Partiu dormir!!");
-  }
+// function acaoDormir() {
+//     console.log("Partiu dormir!!");
+//   }
+const acaoDormir = () => "Partiu dormir!!";
+
 // 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função
 // para que imprima no console o resultado da execução das funções que você criou
 // nos exemplos anteriores.
-function doingThings(callback){
-    callback();
-}
+// function doingThings(callback){
+//     console.log(callback);
+// }
+// const doingThings = (callback) => {
+//   const result = callback();
+//   console.log(result);
+// };
+const doingThings = (callback) => console.log(callback());
 
 doingThings(acaoAcordar);
 doingThings(acaoCafe);
