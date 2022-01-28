c3ffad75b2b82aecc5b99d601f870db8aafd4913
cjheyde <carlaheyde@gmail.com>
2022-01-28 15:40:22 -0300

    copia ex 9.1.1

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/aula-01-JavaScript-Assíncrono-e-Callbacks/ex_9.1.1.js
@@ -0,0 +1,16 @@
+const despesas = [
+    {
+      gym: 99,
+    },
+    {
+      ifood: 200,
+    },
+    {
+      phone: 60,
+    },
+    {
+      internet: 100,
+    },
+  ];
+  
+  const renda = 1000;
