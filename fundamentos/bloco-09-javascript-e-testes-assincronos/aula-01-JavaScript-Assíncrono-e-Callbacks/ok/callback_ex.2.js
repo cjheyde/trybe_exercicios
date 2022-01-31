36b282457dace63fa9e72f44985d6921814d25d4
cjheyde <carlaheyde@gmail.com>
2022-01-31 14:00:02 -0300

    completando callback ex.2

--- fundamentos/bloco-09-javascript-e-testes-assincronos/aula-01-JavaScript-Assíncrono-e-Callbacks/ok/callback_ex.2.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/aula-01-JavaScript-Assíncrono-e-Callbacks/ok/callback_ex.2.js
@@ -3,7 +3,7 @@ const userNationality = ({ firstName, nationality }) => `${firstName} is ${natio
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = () => {
+const getUser = callback => {
   setTimeout(() => {
     const user = {
       firstName: "Ivan",
@@ -12,13 +12,12 @@ const getUser = () => {
     };
     // Retorne a `callback` passada como parâmetro na função `getUser`
     // Dica: você pode manter o `console.log()`
-    console.log(user);
+    console.log(callback(user));
   }, delay());
 };
 
 getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
 getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
-
 
 // 2 - Passe, como parâmetro e como retorno, uma callback para a função getUser .
 // No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa depois de um certo tempo. Complete a função getUser de forma que ela receba uma callback como parâmetro e a retorne para que possa realizar as operações abaixo sobre a pessoa:
