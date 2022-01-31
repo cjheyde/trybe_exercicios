67583143a1d921bea3c85a125321bc87bb19651b
cjheyde <carlaheyde@gmail.com>
2022-01-31 12:13:45 -0300

    reiniciando os exemplos 9.1

--- fundamentos/bloco-09-javascript-e-testes-assincronos/aula-01-JavaScript-Assíncrono-e-Callbacks/testes_assincronos.test.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/aula-01-JavaScript-Assíncrono-e-Callbacks/testes_assincronos.test.js
@@ -31,3 +31,39 @@ test('Não deveria passar!', () => {
 //     }, 500);
 //   });
 
+// Estamos quase lá... O código acima irá gerar um falso-positivo. O motivo é que não estamos passando nenhuma ação a ser realizada quando o erro acontece e "encerramos" nosso teste com o done() sem lançar nenhum erro, dessa forma o teste é interpretado como "correto". Para resolver, podemos passar o parâmetro de erro para o catch . Dessa forma, caso nosso try não consiga executar o código dentro dele, cairá no catch , que vai pegar esse erro e vai encerrar nosso teste lançando um erro.
+
+// test('Não deveria passar!', (done) => {
+//     setTimeout(() => {
+//       try {
+//         expect(10).toBe(5);
+//         console.log('Deveria falhar!');
+//         done();
+//       } catch (error) {
+//         done(error); // Alteramos esta linha
+//       }
+//     }, 500);
+//   });
+
+// Agora sim, nosso teste falhou! Isso significa que você conseguiu testar com sucesso a callback.
+// Vamos a mais um exemplo para fixação, desta vez você irá implementar uma função que espera um tempo e retorna a soma de dois números:
+
+// const asyncSum = (a, b, callback) => {
+//     setTimeout(() => {
+//       const result = a + b;
+//       callback(result);
+//     }, 500);
+//   };
+  
+//   test('Testando asyncSum, soma 5 mais 10', (done) => {
+//     asyncSum(5, 10, (result) => {
+//       try {
+//         expect(result).toBe(15);
+//         done();
+//       } catch (error) {
+//         done(error);
+//       }
+//     });
+//   });
+
+// Quando estiver realizando testes, é muito importante verificar se os resultados exibidos não são falso-positivos. No exemplo acima, em que o teste está passando, experimente mudar a implementação da função asyncSum para que retorne valores incorretos e verifique se o teste irá falhar. Por exemplo, se mudarmos os resultado para ser a + b + 1 o teste falha dizendo que esperava 15 , mas recebeu 16 .
