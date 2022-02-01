9716385f1904852198dc35d63f0cd43d4c177533
cjheyde <carlaheyde@gmail.com>
2022-02-01 01:44:23 -0300

    exercicios 9.2

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/aula-02-javascript-assíncrono-fetch-API-e-async_await/api.js
@@ -0,0 +1,14 @@
+const API_URL = `https://api.coincap.io/v2/assets`
+
+const fetchCripto = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+window.onload = () => fetchJoke();
