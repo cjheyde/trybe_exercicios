ae5b0450cac6ace8f1e6cef11d45e18eff46edcd
cjheyde <carlaheyde@gmail.com>
2022-02-17 13:33:49 -0300

    nao funciona react dentro do git

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex.10.1/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Apresentacao from './Components/Apresentacao';
+
+function App() {
+  return (
+  <Apresentacao />
+  );
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex.10.1/src/Component/Apresentacao.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Apresentacao extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Carla Junger Heyde</h1>
+        <p>
+          Aluna de Desenvolvimento Web na Trybe, maravilhada com todas as
+          pessoas e metodologia!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Apresentacao;
