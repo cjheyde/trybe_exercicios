acb951db247026ab0378f448474b3f7fb7e1db8a
cjheyde <carlaheyde@gmail.com>
2022-04-04 22:33:24 -0300

    update final

--- front-end/bloco-10-introducao-a-react/dia-2-formularios-no-react/exe.10.2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-formularios-no-react/exe.10.2/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 // import Image from './Image';
 import React from 'react';
-// import UserProfile from './UserProfile';
+import UserProfile from './UserProfile';
 import Order from './Order';
 
 class App extends React.Component {
@@ -19,6 +19,21 @@ render() {
   //   email: "amelia@gmail.com",
   //   avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
   // };
+
+  const users = [
+    {
+      id: 104,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    },
+    {
+      id: 79,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    }
+  ];
 
   const headphone = {
       id: 102,
@@ -45,6 +60,7 @@ render() {
      {/* <Image source={'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'} alternativeText="Cute cat staring"/> */}
     {/* <UserProfile user={joao} />
     <UserProfile user={amelia} /> */}
+      {users.map(user => <UserProfile user={user} key={user.id}/>)}
       <h1> Orders recently created: </h1>
         <div>
           <Order order={headphone}/>
