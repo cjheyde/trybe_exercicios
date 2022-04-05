06e28cecad8f7e46f69a9aa51700f0336d61d890
cjheyde <carlaheyde@gmail.com>
2022-04-04 22:59:43 -0300

    cores de fundo para Pokemon e pokedex

---
+++ front-end/bloco-10-introducao-a-react/dia-2-formularios-no-react/my-pokedex/src/components/Pokedex.js
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+import "./Pokedex.css";
+
+class Pokedex extends Component {
+render() {
+  return (
+  <div className="Pokedex">
+    <h1>Pokedex</h1>
+
+    {/* Pokedex : esse componente representa a enciclopédia de pokemons. Esse componente recebe como entrada uma lista de pokemons para serem mostrados na tela. Para cada um desses pokemons recebidos, Pokedex chama o componente Pokemon . */}
+
+  </div>
+  )
+}
+}
+
+export default Pokedex
---
+++ front-end/bloco-10-introducao-a-react/dia-2-formularios-no-react/my-pokedex/src/components/Pokemon.js
@@ -0,0 +1,21 @@
+import React, { Component} from "react";
+import "./Pokemon.css";
+
+class Pokemon extends Component {
+render() {
+  return (
+  <div className="Pokemon">
+    <h1>Pokemon</h1>
+
+  {/* Pokemon : como o próprio nome diz, esse componente representa um pokemon. Esse componente recebe como entrada um objeto que contém informações referentes a um pokemon específico. Esse componente precisa retornar as seguintes informações obrigatórias para serem mostradas para quem usar a aplicação, essas informações devem ser validadas utilizando PropTypes:
+  nome do pokemon;
+  tipo do pokemon;
+  peso médio do pokemon, acompanhado da unidade de medida usada;
+  imagem do pokemon. */}
+
+  </div>
+  )
+}
+}
+
+export default Pokemon
