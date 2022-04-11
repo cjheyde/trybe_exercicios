807fa8f3904d0bf9e53e7f455848dd655c972681
cjheyde <carlaheyde@gmail.com>
2022-04-11 14:36:23 -0300

    updated

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/ex.formulario/src/Form.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/ex.formulario/src/Form.js
@@ -5,65 +5,71 @@ class Form extends Component {
   constructor() {
     super();
 
-    this.handleChangeTextarea = this.handleChangeTextarea.bind(this);
-    this.handleChangeSelect = this.handleChangeSelect.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
 
     this.state = {
       exTextarea: '',
       exSelect: 'coco',
+      email: '',
     };
   }
 
+  // handleInputChange(event) {
+  //   const target = event.target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   const name = target.name;
 
-  handleChangeTextarea(event) {
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // }
+
+    handleInputChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
     this.setState({
-      exTextarea: event.target.value,
+      [name]: value,
     });
   }
 
-  handleChangeSelect(event) {
-    this.setState({
-      exSelect: event.target.value
-    });
-  }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
 
-    this.setState({
-      [name]: value
-    });
-  }
 
   render() {
     return (
       <div>
         <h1>Exercícios 11.2</h1>
         <form className="form">
-          <label>
+
+        <fieldset>
+
+          <label htmlFor="ano_nascimento">
           Ano de Nascimento:
           <input
-            type="number"
+            id="ano_nascimento"
             name="ano_nascimento"
+            type="number"
             value={this.state.birthYear}
             onChange={this.handleInputChange} 
           />
           </label>
+
           <br/>
-          <label>
+
+          <label htmlFor="aprendi">
           <input
-            type="checkbox"
+            id="aprendi"
             name="aprendi"
+            type="checkbox"
             value={this.state.aprendi}
             onChange={this.handleInputChange} 
           />
           Aprendi!
           </label>
+          </fieldset>
           <br/>
-          <label>
+          <fieldset>
+          <label htmlFor="nome">
           Seu Nome:
           <input
             type="text"
@@ -73,7 +79,7 @@ class Form extends Component {
           />
           </label>
           <br/>
-          <label>
+          <label htmlFor="isGoing">
           <input
             name="isGoing"
             type="checkbox"
@@ -83,7 +89,7 @@ class Form extends Component {
           Estão indo
           </label>
           <br/>
-          <label>
+          <label htmlFor="numberOfGuests">
           Número de convidados:
           <input
             name="numberOfGuests"
@@ -93,21 +99,32 @@ class Form extends Component {
           </label>
           <br/>
           <br/>
-          <label>
+          <label htmlFor="exTextarea">
           Exemplo de TextArea:
-          <textarea name="exTextarea" value={this.state.exTextarea} onChange={this.handleChangeTextarea} />
+          <textarea name="exTextarea" value={this.state.exTextarea} onChange={this.handleInputChange} />
           </label>
           <br/>
           <br/>
-          <label>
+          <label htmlFor="exSelect">
           Exemplo de Select:
-          <select value={this.state.exSelect} onChange={this.handleChangeSelect}>
+          <select name="exSelect" value={this.state.exSelect} onChange={this.handleInputChange}>
             <option value="laranja">Laranja</option>
             <option value="limao">Limão</option>
             <option value="coco">Coco</option>
             <option value="manga">Manga</option>
           </select>
           </label>
+          <label htmlFor="email">
+          Email:
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={ this.state.email }
+            onChange={ this.handleInputChange }
+          />
+          </label>
+        </fieldset>
         </form>
       </div>
     );
